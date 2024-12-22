6b8fa56eebf44e67fbd34b19ec0c8aef6c5165a7
GiriMalayavantham <girimalyavantham99@gmail.com>
2024-12-22 21:52:42 +0530

    2nd commit

--- app1/views.py
+++ app1/views.py
@@ -28,7 +28,7 @@ def cart_view(request):
     qty=request.GET['qty']
     if pname in request.session:
       del request.session[pname]
-      msg="Product Deleted from cart"
+      msg="<h2>Product Deleted from cart</h2>"
     else:
       msg="Invalid ProductName"
     response=render(request,'app1/index.html',context={'msg':msg})
